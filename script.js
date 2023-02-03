191d86d2a57ba8d198c207606e7bb94ce009bea0
Nícolas de Oliveira Macedo <106557745+Nogc1@users.noreply.github.com>
2023-02-03 16:52:55 -0300

    adicionando um novo botão

--- script.js
+++ script.js
@@ -1,88 +1,124 @@
+// Botões de farm
 let contador = document.getElementById('contador')
-let clic = document.getElementById('clique')
-let auto = document.getElementById('autoclick')
+let chute_html = document.getElementById('chute')
+let passe_html = document.getElementById('passe')
+let chuteira_html = document.getElementById('chuteira')
 
-var valor_click = 10
-var valor_auto = 20
+var valor_chute = 10
+var valor_passe = 20
+var valor_chuteira = 50
 
-var clique_auto = 0
-var click = 0
+var passe = 0
+var chutes = 0
+var chuteira = 0
 
 var gols = 0
 var gols_valor = 1
 
-setInterval(aut, 1000 )
+// Atualiza o contador e as variaveis para o novo valor através da função aut
+setInterval(aut, 1000)
 
 contador.innerHTML = `<p>Gols: ${gols.toFixed(0)}</p>`
-clic.disabled = true
-auto.disabled = true
+chute_html.disabled = true
+passe_html.disabled = true
+chuteira_html.disabled = true
 
+// Inicia o contador
 function contagem() {
     gols += gols_valor
     contador.innerHTML =`<p>Gols: ${gols.toFixed(0)}</p>`
 }
 
-function clique() {
-    gols -= valor_click
-    if (gols < valor_click) {
-        clic.disabled = true
+// Verifica se o botão foi apertado e qual que foi apertado
+function clique(clicked) {
+    if (clicked == 'chute') {
+        gols -= valor_chute
+        if (gols < valor_chute) {
+            chute_html.disabled = true
+        }
+    
+        if (gols < valor_passe) {
+            passe_html.disabled = true
+        }
+    
+        valor_chute *= 1.2
+        chutes += 3
+        gols_valor = chutes
+    
+        if (gols < 0) {
+            gols = 0
+        }
     }
+    else if (clicked == 'chuteira') {
+        gols -= valor_chuteira
+        if (gols < valor_chute) {
+            chute_html.disabled = true
+        }
+        if (gols < valor_passe) {
+            passe_html.disabled = true
+        }
+        if (gols < valor_chuteira) {
+            chuteira_html.disabled = true
+        }
 
-    if (gols < valor_auto) {
-        auto.disabled = true
+        valor_chuteira *= 1.2
+        chuteira += 2
+
+        if (gols < 0) {
+            gols = 0
+        }
     }
+    else if (clicked == 'passe') {
+        gols -= valor_passe
+        if (gols < valor_passe) {
+            passe_html.disabled = true
+        }
 
-    valor_click = valor_click * 1.2
-    click += 3
-    gols_valor = click
+        if (gols < valor_chute) {
+            chute_html.disabled = true
+        }
 
-    if (gols < 0) {
-        gols = 0
-    }
+        passe += 1
+        valor_passe *= 1.1
 
-    contador.innerHTML =`<p>Gols: ${gols.toFixed(0)}</p>`
-}
-
-function autoclick() {
-    gols -= valor_auto
-    if (gols < valor_auto) {
-        auto.disabled = true
-    }
-
-    if (gols < valor_click) {
-        clic.disabled = true
-    }
-
-    clique_auto += 1
-    valor_auto = valor_auto * 1.1
-
-    if (gols < 0) {
-        gols = 0
+        if (gols < 0) {
+            gols = 0
+        }
     }
 
     contador.innerHTML =`<p>Gols: ${gols.toFixed(0)}</p>`
 }
 
 function aut() {
-    if (gols >= valor_click) {
-        clic.disabled = false
+    if (gols >= valor_chute) {
+        chute_html.disabled = false
     }
 
-    if (gols >= valor_auto) {
-        auto.disabled = false
+    if (gols >= valor_passe) {
+        passe_html.disabled = false
     }
 
-    if (gols < valor_click) {
-        clic.disabled = true
+    if (gols >= valor_chuteira) {
+        chuteira_html.disabled = false
+    }
+
+    if (gols < valor_chute) {
+        chute_html.disabled = true
     }
     
-    if (gols < valor_auto) {
-        auto.disabled = true
+    if (gols < valor_passe) {
+        passe_html.disabled = true
     }
 
-    gols += clique_auto
+    if (gols < valor_chuteira) {
+        chuteira_html.disabled = true
+    }
 
-    clic.value = `Clique ${valor_click.toFixed(0)}`
-    auto.value = `Autoclick ${valor_auto.toFixed(0)}`
+    gols += passe
+    gols += chuteira
+
+    chute_html.value = `Chute ${valor_chute.toFixed(0)}`
+    passe_html.value = `Passe ${valor_passe.toFixed(0)}`
+    chuteira_html.value = `Chuteira ${valor_chuteira.toFixed(0)}`
     contador.innerHTML =`<p>Gols: ${gols.toFixed(0)}</p>`
 }
